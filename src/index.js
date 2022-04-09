c34c160b23916f6bc44e88e96e92679f2560915e
ArinKistia22 <arinkistian22@gmail.com>
2022-04-09 20:57:09 +0700

    praktikum 6

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import * as ReactDOMClient from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import MainReducer from './reducers/MainReducer';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import CreateTodo from './containers/CreateTodo';
+
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f =>
+  f)(createStore)(MainReducer)
+
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
+
+root.render(<Provider store={store}> <CreateTodo /> </Provider>);
+
+// ReactDOM.render(<Provider store={store}>
+//   <CreateTodo />
+//   </Provider>
+//   , document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
